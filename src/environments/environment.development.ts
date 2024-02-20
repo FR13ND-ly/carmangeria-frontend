3fff4ad3c4ecda07444e338d910916b880f578cc
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2024-02-20 18:08:11 +0200

    fix api, optimize responsivity

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // apiUrl: 'http://127.0.0.1:8000/',
-  apiUrl: 'https://stingray-app-7bc69.ondigitalocean.app/api/',
+  apiUrl: 'https://stingray-app-7bc69.ondigitalocean.app/',
 };
