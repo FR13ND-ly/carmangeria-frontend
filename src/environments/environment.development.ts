1d4b838c2389d1ef302ad3caa1480bfc6b84e70b
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2024-04-08 17:23:37 +0300

    fix api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:8000/',
-  // apiUrl: 'https://sea-lion-app-kexjh.ondigitalocean.app/',
+  // apiUrl: 'http://127.0.0.1:8000/',
+  apiUrl: 'https://sea-lion-app-kexjh.ondigitalocean.app/',
 };
